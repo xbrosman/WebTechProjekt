6f0f0423ea9869e742357223d3d1bcfa4ceb7ada
David-Baranec <xbaranecd@stuba.sk>
2022-05-20 14:42:01 +0200

    feat: app blade added + styling

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,35 @@
-<!DOCTYPE html>
+       
+<x-app-layout>
+    <x-slot name="header">
+        <br>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Welcome') }}
+        </h2>
+        <br>
+    </x-slot>
+    <form action="/octave" method="POST">
+            @csrf
+            <textarea class="form-control" id="input" name="query" rows="7"></textarea>
+           
+            <button type="submit"  class="btn btn-primary btn-lg btn-block">{{__('Send')}}</button>
+         
+    </form>
+    
+    <textarea class="form-control" type="text" id="output" name="output" readonly rows="7">
+            @if ($response != null)
+                {{ $response }}
+            @endif
+        </textarea>
+    <x-slot name="footer">
+        <div class="text-center p-3">
+                © 2022 Copyright: Andrašovič, Baranec, Brosman, Teplanský
+
+        </div>
+    </x-slot>
+</x-app-layout>
+
+
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -6,9 +37,9 @@
 
         <title>Graph</title>
 
-        
+       
     </head>
-    <body class="antialiased">
+    <body class="p-3 mb-2 bg-dark text-white">
            <h1>Octave output</h1>
             
 
@@ -25,4 +56,4 @@
             @endif
         </textarea>
     </body>
-</html>
+</html> -->
