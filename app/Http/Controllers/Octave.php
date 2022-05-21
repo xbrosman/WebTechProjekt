588e81d7b127b850e47ca410d1a996d1b3b0501f
Alex Teplansky <xteplansky@stuba.sk>
2022-05-21 20:26:16 +0200

    feat:making logs

--- app/Http/Controllers/Octave.php
+++ app/Http/Controllers/Octave.php
@@ -4,20 +4,16 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Config;
 
 class Octave extends Controller
 {
     public function readOctave(Request $request){
         return view("successLogin")->with(["response"=> null]);
     }
-    
+
     public function updateOctave(Request $request)
     {
-        if (strcmp($request->api_key, Config::get('app.api_key')))
-            return "Wrong API KEY!";
-
-        $query = $request->input('query');   
+        $query = $request->input('query');
         //return var_dump($query);
         exec('octave');
         exec('octave --eval "pkg load control;' . $query . '"', $response);
