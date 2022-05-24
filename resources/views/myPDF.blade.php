fe142687af9d4a55ab32c47d55ec0934b55a41e5
David-Baranec <xbaranecd@stuba.sk>
2022-05-24 15:55:41 +0200

    feat: docker-comp update

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hi</title>
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
 </head>
 <body>
-    <h1>{{ $title }}</h1>
+    <h1 class="text-center">{{ $title }}</h1>
     <p>{{ $date }}</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
