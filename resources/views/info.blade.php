69e3e998e398c9a3b3489a2675253108107a56d3
David-Baranec <xbaranecd@stuba.sk>
2022-05-24 17:05:32 +0200

    fix: Info page updated and missspelling solved

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -19,12 +19,12 @@
                 <li class="list-group-item list-group-item-dark"><strong>{{ __('Animation') }}</strong>: Andrej Andrašovič <strong>{{ __('Not completed') }}</strong> </li>
                 <li class="list-group-item list-group-item-dark"><strong>{{ __('Graph synchronized with animation') }}</strong>: Andrej Andrašovič, Alex Teplanský <strong>{{ __('Not completed') }}</strong></li>
                 <li class="list-group-item list-group-item-dark"><strong>{{ __('Check API form') }}</strong>:  Dávid Baranec,Filip Brosman</li>
-                <li class="list-group-item list-group-item-dark"><strong>{{ __('Logs exported to CSV + sending mail') }}</strong>:Alex Teplanský <strong>{{ __('Not completed') }}</strong></li>
-                <li class="list-group-item list-group-item-dark"><strong>{{ __('API description export to PDF') }}</strong>: Dávid Baranec, Alex Teplanský <strong>{{ __('Not completed') }}</strong></li>
+                <li class="list-group-item list-group-item-dark"><strong>{{ __('Logs exported to CSV + sending mail') }}</strong>:Alex Teplanský </li>
+                <li class="list-group-item list-group-item-dark"><strong>{{ __('API description export to PDF') }}</strong>: Dávid Baranec </li>
                 <li class="list-group-item list-group-item-dark"><strong>{{ __('Synchronization access of multiple users') }}</strong>: {{ __('All members') }} <strong>{{ __('Not completed') }}</strong></li>
                 <li class="list-group-item list-group-item-dark"><strong>{{ __('Docker wrapper') }}</strong>: Filip Brosman, Andrej Andrašovič </li>
                 <li class="list-group-item list-group-item-dark"><strong>{{ __('Using github') }}</strong>: {{ __('All members') }} </li>
-                <li class="list-group-item list-group-item-dark"><strong>{{ __('Finalization') }}</strong>: <strong>{{ __('Not completed') }}</strong></li>
+                <li class="list-group-item list-group-item-dark"><strong>{{ __('Finalization') }}</strong>: {{ __('All members') }}</strong></li>
                 
             </ul>
         </div>
