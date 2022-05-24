ba40a5799a39bb32d155c9112cb423924910b23d
Alex Teplansky <xteplansky@stuba.sk>
2022-05-24 15:02:00 +0200

    feat: sending mail to webtechprojekt2022@gmail.com

--- routes/web.php
+++ routes/web.php
@@ -37,13 +37,11 @@ Route::post('/main/checkLogin', [MainController::class, 'checkLogin']);
 Route::get('/main/successLogin', [MainController::class, 'successLogin']);
 Route::get('/main/logout', [MainController::class, 'logout']);
 
-
 Route::get('/registration', [RegistrationController::class, 'readRegistration']);
 Route::post('/registration', [RegistrationController::class, 'createRegistration']);
 
 Route::get('/csv', [Csv::class, 'createCsv']);
-
-
+Route::get('/csv/mail', [Csv::class, 'sendCsv']);
 
 Route::get('/registration', [RegistrationController::class, 'readRegistration']);
 Route::post('/registration', [RegistrationController::class, 'createRegistration']);
