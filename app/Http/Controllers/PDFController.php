f97fbf70abc7306f645cb2dc16e0b47b593dcd25
David-Baranec <xbaranecd@stuba.sk>
2022-05-24 15:44:15 +0200

    feat: API description sending

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PDF;
+
+class PDFController extends Controller
+{  
+    public function generatePDF()
+    {
+        $data = [
+            'title' => __('API description') ,
+            'date' => date('m/d/Y')
+        ];
+          
+        $pdf = PDF::loadView('myPDF', $data);
+    
+        return $pdf->download(__('API description').'.pdf');
+    }
+}
