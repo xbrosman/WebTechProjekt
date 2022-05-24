9578ba812aa846de5322ed23df55a026873ab11b
Andrej Andrašovič <andrej.andrasovic@gmail.com>
2022-05-24 16:56:20 +0200

    feat: implement animation konva.js

--- resources/js/animation.js
+++ resources/js/animation.js
@@ -47,3 +47,58 @@ var interval = setInterval(function () {
         clearInterval(interval)
     }
 }, 1);
+
+var stage = new Konva.Stage({
+    container: 'container',
+    width: 600,
+    height: 600,
+});
+
+var layer = new Konva.Layer();
+
+var tire = new Konva.Circle({
+    x: 200,
+    y: 250,
+    radius: 40,
+    fill: 'black'
+});
+var road = new Konva.Rect({
+    x: 0,
+    y: 300,
+    width: 400,
+    height: 10,
+    fill: 'green',
+});
+var car = new Konva.Rect({
+    x: 100,
+    y: 130,
+    width: 200,
+    height: 50,
+    fill: 'blue',
+});
+
+// layer.add(road);
+layer.add(car);
+
+// add the triangle shape to the layer
+layer.add(tire);
+
+// add the layer to the stage
+stage.add(layer);
+
+var carY = 130;
+var m = 0;
+var anim = new Konva.Animation(function (frame) {
+    car.y(
+        carY - ((x1[m]) * 1000)
+    );
+    tire.y(
+        carY - ((x1[m]) * 1000) - (x3[m]) * 10000 + 120
+    );
+    m++;
+    if (m > 480) {
+        anim.stop();
+    }
+}, layer);
+
+anim.start();
